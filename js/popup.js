049983a9d9f6676ed1d3f3a1ef7c1aca8a9d8869
haoliang.wu <haoliang.wu@liferay.com>
2015-06-30 16:57:44 +0800

    toggle team select form

--- js/popup.js
+++ js/popup.js
@@ -4,16 +4,26 @@ $(document).ready(function () {
 
     $(".team select").change(function () {
         var value = $(this).val();
+
+        if (value =='qar') {
+            $("#fixpack").hide();
+            $("#qa-r").show();
+        }
+        else {
+            $("#qa-r").hide();
+            $("#fixpack").show();
+        }
+
         //console.log(value);
         chrome.storage.local.set({"team": value}, function () {
             console.log("Set team to %s", value);
         });
     });
 
-    $(".parameter select").change(function() {
+    $(".parameter select").change(function () {
         var value = $(this).val();
 
-        chrome.storage.local.set({"portal_branch":value},function() {
+        chrome.storage.local.set({"portal_branch": value}, function () {
             console.log("Set portal_branch to %s", value);
         })
     });
@@ -23,9 +33,17 @@ $(document).ready(function () {
 function init() {
     chrome.storage.local.get('team', function (result) {
         $(".team select").val(result.team);
+        if (result.team == 'qar') {
+            $("#fixpack").hide();
+            $("#qa-r").show();
+        }
+        else {
+            $("#qa-r").hide();
+            $("#fixpack").show();
+        }
     });
 
-    chrome.storage.local.get('portal_branch',function(result) {
+    chrome.storage.local.get('portal_branch', function (result) {
         console.log(result.portal_branch);
         $(".parameter select").val(result.portal_branch);
     });
