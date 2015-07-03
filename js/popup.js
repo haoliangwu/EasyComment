f70ebfb4d133d04c6dc2fd2eb56d25689645e94e
haoliang.wu <haoliang.wu@liferay.com>
2015-07-03 10:29:38 +0800

    Revert "toggle team select form"
    This reverts commit 049983a9d9f6676ed1d3f3a1ef7c1aca8a9d8869.
    
    Conflicts:
    	js/popup.js
    	popup.html

--- js/popup.js
+++ js/popup.js
@@ -18,11 +18,19 @@ $(document).ready(function () {
             $("#fixpack").show();
         }
 
+        //console.log(value);
         chrome.storage.local.set({"team": value}, function () {
             console.log("Set team to %s", value);
         });
     });
 
+    $(".parameter select").change(function() {
+        var value = $(this).val();
+
+        chrome.storage.local.set({"portal_branch":value},function() {
+            console.log("Set portal_branch to %s", value);
+        })
+    });
 })
 
 function init() {
@@ -97,5 +105,11 @@ function init_fixpack() {
 
     chrome.storage.local.get('portal_branch', function (result) {
         $(".parameter_fixpack select").val(result.portal_branch);
+        $(".team select").val(result.team);
+    });
+
+    chrome.storage.local.get('portal_branch',function(result) {
+        console.log(result.portal_branch);
+        $(".parameter select").val(result.portal_branch);
     });
 }
